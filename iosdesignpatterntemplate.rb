b61fa3e62fe0e4a2da2c03f39502870494a7ece9
Imran Hishaam <ImranHishaam@users.noreply.github.com>
2017-03-27 19:31:44 +0530

    Update iosdesignpatterntemplate.rb

--- iosdesignpatterntemplate.rb
+++ iosdesignpatterntemplate.rb
@@ -11,7 +11,18 @@ class Iosdesignpatterntemplate < Formula
   # depends_on "cmake" => :build
 
   def install
-    bin.install "viperscript"    
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+     system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+    
+    bin.install "viperscript"
+    
   end
 
   test do
